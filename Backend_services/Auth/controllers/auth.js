1bed6ad9d3bfa958e6f4369d8fb24dd0c6aa5ddb
Adeyemo Peter <peteradeyemo66@gmail.com>
2022-08-04 22:00:45 +0100

    refactored user controller

--- Backend_services/Auth/controllers/auth.js
+++ Backend_services/Auth/controllers/auth.js
@@ -1,20 +1,12 @@
 const User = require('../models/user')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../../errors')
-const bcrypt = require('bcryptjs')
+
 
 const register = async(req,res) => {
-    const { name, email, password } = req.body
-    if(!name || !email || !password){
-        throw new BadRequestError('please provide, name, email and password')
-    }
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-
-    const tempUser = { name, email, password: hashedPassword }
-
-    const user = await User.create({...tempUser})
     
+
+    const user = await User.create({...req.body})
+
     res.status(StatusCodes.CREATED).json({user})
 }
 const login = async(req,res) => {
--- Backend_services/Auth/models/user.js
+++ Backend_services/Auth/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -22,6 +23,11 @@ const UserSchema = new mongoose.Schema({
         required:[true,'please provide password'],
         minlength: 6
     }
+})
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 module.exports = mongoose.model('User', UserSchema)
