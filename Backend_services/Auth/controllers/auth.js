8d2327255973f33a535439d426a5d27991ce1476
Adeyemo Peter <peteradeyemo66@gmail.com>
2022-08-04 15:03:18 +0100

    brcypt enabled

--- Backend_services/Auth/controllers/auth.js
+++ Backend_services/Auth/controllers/auth.js
@@ -1,6 +1,12 @@
 const User = require('../models/user')
 const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../../errors')
+
 const register = async(req,res) => {
+    const { name, email, password } = req.body
+    if(!name || !email || !password){
+        throw new BadRequestError('please provide, name, email and password')
+    }
     const user = await User.create({...req.body})
     res.status(StatusCodes.CREATED).json({user})
 }
