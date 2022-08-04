ecd3fcdcb1248d30eda96b6e19c1334702d6745d
Adeyemo Peter <peteradeyemo66@gmail.com>
2022-08-04 13:50:15 +0100

    user schema succcessfully created in mongoose

--- Backend_services/Auth/models/user.js
+++ Backend_services/Auth/models/user.js
@@ -3,13 +3,25 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
     name: {
         type:String,
-        maxlength:[20,'name cannot be more than 20 characters'],
-        required:[true,'must provide name'], 
+        maxlength:[50,'name cannot be more than 50 characters'],
+        minlength:3,
+        required:[true,'please provide name'], 
         trim:true
+    },
+    email: {
+        type:String,
+        required:[true,'please provide email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
+          ],
+        unique: true,
     },
     password: {
         type:String,
-        default: false
+        required:[true,'please provide password'],
+        minlength: 6,
+        maxlength: 12
     }
 })
 
--- Backend_services/app.js
+++ Backend_services/app.js
@@ -22,6 +22,10 @@ app.use((req, res, next) => {
     next();
   });
 
+//router
+const authRouter = require('./Auth/routes/auth')
+const postRouter = require('./Posts/routes/post')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -29,15 +33,18 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 //app.use(express.static('./public'))
 app.use(express.json())
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/posts', postRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 //routes
 
 
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
