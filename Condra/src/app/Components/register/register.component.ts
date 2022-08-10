ac96ee5f184d312d6ffe3117a0cf4e543e784e49
Adeyemo Peter <peteradeyemo66@gmail.com>
2022-08-10 15:21:19 +0100

    UX: ngrx actions added to signup functionality

--- Condra/src/app/Components/register/register.component.ts
+++ Condra/src/app/Components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,21 +7,33 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  email = new FormControl('', [Validators.required, Validators.email]);
-  name = new FormControl('', [Validators.required]);
+  form: FormGroup;
   hide = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl('', {
+        validators: [Validators.required]
+      }),
+      email: new FormControl('', {
+        validators: [Validators.required]}),
+      password: new FormControl('', {
+        validators: [Validators.required]}),
+    });
   }
   
   getErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.form.value.email.hasError('required')) {
       return 'You must enter a value';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.form.value.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
   }
 }
 
