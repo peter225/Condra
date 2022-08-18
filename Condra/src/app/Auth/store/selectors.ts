51b17cfe2d874ab13275ee56dbabd9805b95e2fc
Adeyemo Peter <peteradeyemo66@gmail.com>
2022-08-18 11:18:02 +0100

    CI: creating selectors

--- Condra/src/app/Auth/store/selectors.ts
+++ Condra/src/app/Auth/store/selectors.ts
@@ -1,3 +1,7 @@
 import { createFeatureSelector } from "@ngrx/store";
+import { AppStateInterface } from "src/app/shared/types/appState.interface";
+import { AuthStateInterface } from "src/app/shared/types/authState.interface";
 
-export const authFeatureSelector = createFeatureSelector('auth')
+export const authFeatureSelector = createFeatureSelector<
+    AppStateInterface,
+    AuthStateInterface>('auth')
