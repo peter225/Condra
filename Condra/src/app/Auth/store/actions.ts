ccc1956896abcf4971f277a2b4ab173a6af3f074
Adeyemo Peter <peteradeyemo66@gmail.com>
2022-08-18 14:24:58 +0100

    more actions initiated

--- Condra/src/app/Auth/store/actions.ts
+++ Condra/src/app/Auth/store/actions.ts
@@ -1,8 +1,16 @@
 import { createAction, props } from "@ngrx/store";
 import { ActionTypes } from './actionTypes';
 import { RegisterRequestInterface } from 'src/app/shared/types/register-request.interface';
+import { CurrentUserInterface } from "src/app/shared/types/current-user.interface";
 
 export const registerAction = createAction(
     ActionTypes.REGISTER,
     props<{request: RegisterRequestInterface}>()
 )
+
+export const registerSuccessAction = createAction(
+    ActionTypes.REGISTER_SUCCESS,
+    props<{currentUser: CurrentUserInterface}>()
+)
+
+export const registerFailureAction = createAction(ActionTypes.REGISTER_FAILURE)
