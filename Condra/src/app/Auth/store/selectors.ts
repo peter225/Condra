891191fd3b132454806aa3b91ab23e93ddf54815
Adeyemo Peter <peteradeyemo66@gmail.com>
2022-08-18 11:53:49 +0100

    Component selection from store

--- Condra/src/app/Auth/store/selectors.ts
+++ Condra/src/app/Auth/store/selectors.ts
@@ -1,7 +1,12 @@
-import { createFeatureSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { AppStateInterface } from "src/app/shared/types/appState.interface";
 import { AuthStateInterface } from "src/app/shared/types/authState.interface";
 
 export const authFeatureSelector = createFeatureSelector<
-    AppStateInterface,
-    AuthStateInterface>('auth')
+AuthStateInterface>('auth')
+
+
+export const isSubmittingSelector = createSelector(
+    authFeatureSelector, 
+    (authState: AuthStateInterface)=> authState.isSubmitting
+)
--- Condra/src/app/Components/register/register.component.ts
+++ Condra/src/app/Components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder,Validators } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { isSubmittingSelector } from 'src/app/Auth/store/selectors';
 import { registerAction } from '../../Auth/store/actions';
 @Component({
   selector: 'app-register',
@@ -9,11 +11,17 @@ import { registerAction } from '../../Auth/store/actions';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
   hide = true;
 
   constructor(private store:Store) { }
 
   ngOnInit(): void {
+   this.initializeForm() 
+   this.initializeValues()
+  }
+
+  initializeForm(): void {
     this.form = new FormGroup({
       username: new FormControl('', {
         validators: [Validators.required]
@@ -24,6 +32,11 @@ export class RegisterComponent implements OnInit {
         validators: [Validators.required]}),
     });
   }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
+    console.log('isSubmitting', this.isSubmitting$)
+  }
   
   getErrorMessage() {
     if (this.form.value.email.hasError('required')) {
@@ -32,6 +45,8 @@ export class RegisterComponent implements OnInit {
 
     return this.form.value.email.hasError('email') ? 'Not a valid email' : '';
   }
+
+
 
   onSubmit(): void {
     console.log(this.form.value);
