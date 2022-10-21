fd9f8151578413803f398bc3aebeda1fa1afe12d
peter <peteradeyemo66@gmail.com>
2022-10-21 10:58:54 +0100

    CI: reducers update for login states

--- Condra/src/app/Auth/store/reducer.ts
+++ Condra/src/app/Auth/store/reducer.ts
@@ -2,6 +2,7 @@ import { AuthStateInterface } from "src/app/Auth/Types/authState.interface";
 import { createReducer, on, Action } from '@ngrx/store';
 
 import { registerAction, registerFailureAction, registerSuccessAction } from './actions/actions';
+import { LoginAction, LoginFailureAction, LoginSuccessAction } from './actions/login-action';
 
 const initialState: AuthStateInterface = {
     isSubmitting: false,
@@ -31,6 +32,34 @@ const authReducer = createReducer(
     on(
         registerFailureAction,
         (state, action): AuthStateInterface => ({
+            ...state,
+            isSubmitting: false,
+            validationErrors: action.errors
+        })
+    ),
+    on(
+        LoginAction,
+        (state: AuthStateInterface) => ({
+            ...state,
+            isSubmitting: true,
+            validationErrors: null
+        })
+
+    ),
+    on(
+        LoginSuccessAction,
+        (state,action): AuthStateInterface => ({
+            ...state,
+            isSubmitting: false,
+            isLoggedIn: true,
+            currentUser: action.currentUser,
+            validationErrors: null
+        })
+
+    ),
+    on(
+        LoginFailureAction,
+        (state, action): AuthStateInterface =>({
             ...state,
             isSubmitting: false,
             validationErrors: action.errors
