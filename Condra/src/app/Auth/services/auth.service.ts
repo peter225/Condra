9d1f7f465acd25bfc4f754f3b441b2bbc9c23bce
peter <peteradeyemo66@gmail.com>
2022-10-21 10:29:06 +0100

    auth services updated

--- Condra/src/app/Auth/services/auth.service.ts
+++ Condra/src/app/Auth/services/auth.service.ts
@@ -14,14 +14,19 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  getUser(response: AuthResponseInterface): CurrentUserInterface{
+    return response.user
+  }
+
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface>{
     const url = environment.url + '/auth/register';
     return this.http.post<AuthResponseInterface>(url, data).pipe(map(
-      (response: AuthResponseInterface)=> response.user))
+      this.getUser))
   }
   login(data: LoginRequestInterface): Observable<CurrentUserInterface>{
     const url = environment.url + '/auth/login';
-    return this.http.post<AuthResponseInterface>(url, data).pipe(map(
-      (response: AuthResponseInterface)=> response.user))
+    return this.http
+                .post<AuthResponseInterface>(url, data)
+                  .pipe(map(this.getUser))
   }
 }
