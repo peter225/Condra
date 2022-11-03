7ffff80d799a8181d2d429a46f4231310524df06
peter <peteradeyemo66@gmail.com>
2022-11-03 23:12:24 +0100

    actions perfectly created

---
+++ Condra/src/app/shared/modules/feed/store/actions/getFeed.action.ts
@@ -0,0 +1,17 @@
+import { createAction, props } from "@ngrx/store";
+import { GetFeedResponseInterface } from "../../types/getFeedResponse.interface";
+import { ActionTypes } from '../actionTypes';
+
+export const getFeedAction = createAction(
+    ActionTypes.GET_FEED,
+    props<{url: string}>()
+)
+
+export const getFeedSuccessAction = createAction(
+    ActionTypes.GET_FEED_SUCCESS,
+    props<{feed: GetFeedResponseInterface}>()
+)
+
+export const getUserFailureAction = createAction(
+    ActionTypes.GET_FEED_FAILURE
+)
