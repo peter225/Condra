fe564aa4abf43f9797b5132ab67381d0079cc397
peter <peteradeyemo66@gmail.com>
2022-11-03 22:59:01 +0100

    feed services up and running

--- Condra/src/app/shared/modules/feed/feed.module.ts
+++ Condra/src/app/shared/modules/feed/feed.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedComponent } from './Components/feed/feed.component';
+import { FeedService } from './services/feed.service';
 
 
 
@@ -13,6 +14,9 @@ import { FeedComponent } from './Components/feed/feed.component';
   ],
   exports: [
     FeedComponent
+  ],
+  providers: [
+    FeedService
   ]
 })
 export class FeedModule { }
--- Condra/src/app/shared/modules/feed/services/feed.service.ts
+++ Condra/src/app/shared/modules/feed/services/feed.service.ts
@@ -1,14 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { GetFeedResponseInterface } from '../types/GetFeedResponse.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getFeed(url: string): Observable<any>{
-    return '';
+  getFeed(url: string): Observable<GetFeedResponseInterface>{
+    const completeUrl = environment.apiUrl + url;
+    return this.http.get<GetFeedResponseInterface>(completeUrl);
   }
 }
