97ac6caeabef3d5bba79c4fc111b7374d1495e30
peter <peteradeyemo66@gmail.com>
2022-11-03 21:52:59 +0100

    feed component created

--- Condra/src/app/shared/modules/feed/feed.module.ts
+++ Condra/src/app/shared/modules/feed/feed.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FeedComponent } from './Components/feed/feed.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FeedComponent
+  ],
   imports: [
     CommonModule
   ]
