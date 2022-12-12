f09ab772135825cfec4984f4a50ee63677e5c8ce
peter <peteradeyemo66@gmail.com>
2022-12-12 07:54:55 +0100

    CI: Implementation of popular tags

--- Condra/src/app/shared/modules/popularTags/popular-tags/popular-tags.module.ts
+++ Condra/src/app/shared/modules/popularTags/popular-tags/popular-tags.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PopularTagsService } from '../services/popular-tags.service';
 
 
 
@@ -7,6 +8,7 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  providers:[PopularTagsService]
 })
 export class PopularTagsModule { }
--- Condra/src/app/shared/modules/popularTags/services/popular-tags.service.ts
+++ Condra/src/app/shared/modules/popularTags/services/popular-tags.service.ts
@@ -1,9 +1,21 @@
+import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PopularTagsService {
 
-  constructor() { }
+  private http: HttpClient
+  constructor(private handler: HttpBackend) {
+                this.http = new HttpClient(handler)
+  }
+
+  getPopularTags(): Observable<string[]>{
+    return this.http.get(environment.apiUrl+'/tags').pipe(map((response:any)=>{
+      return response.tags
+    }))
+  }
 }
