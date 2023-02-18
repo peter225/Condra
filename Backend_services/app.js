4764714dd064c5431a974ddcfe7a10e8a85e9a57
peter <peteradeyemo66@gmail.com>
2023-02-18 14:38:15 +0100

    CI: articles instead of post

--- Backend_services/app.js
+++ Backend_services/app.js
@@ -30,7 +30,7 @@ app.use(cors());
 app.use(xss());
 //router
 const authRouter = require('./Auth/routes/auth')
-const postRouter = require('./Posts/routes/post')
+const articleRouter = require('./Articles/routes/article')
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -40,7 +40,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 //app.use(express.json())
 
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/posts',authorizeUser, postRouter)
+app.use('/api/v1/articles',authorizeUser, articleRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
