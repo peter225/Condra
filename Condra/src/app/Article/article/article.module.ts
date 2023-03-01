8eadaf5c2dce4c4fbc8e1165300c46da1dd4d30d
peter <peteradeyemo66@gmail.com>
2023-03-01 15:02:53 +0100

    CI: article interface completed

--- Condra/src/app/Article/article/article.module.ts
+++ Condra/src/app/Article/article/article.module.ts
@@ -8,6 +8,8 @@ import { reducers } from '../store/reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TagListModule } from 'src/app/shared/modules/tagList/tag-list/tag-list.module';
 
 const routes = [
   {
@@ -23,7 +25,9 @@ const routes = [
     MatIconModule,
     RouterModule.forChild(routes),
     EffectsModule.forFeature([GetArticleEffect]),
-    StoreModule.forFeature('article',reducers)
+    StoreModule.forFeature('article',reducers),
+    MatProgressSpinnerModule,
+    TagListModule
   ],
   providers:[ArticleService]
 })
